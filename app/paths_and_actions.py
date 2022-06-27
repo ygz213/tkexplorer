72e79e57f6079b51f1994c872b5436c937e554dd
ygz213 <yigitgurbuz0630@outlook.com>
2022-06-27 21:25:47 +0300

    Added move up button

--- app/paths_and_actions.py
+++ app/paths_and_actions.py
@@ -1,4 +1,5 @@
 from os import chdir, getcwd, path, walk
+from tkinter import ttk
 import content_buttons as cb
 
 FIRST_TIME = True        # Boolean to check if it is the first time path_updater is working
@@ -15,6 +16,12 @@ def path_updater(stringvar, entry):
 def list_folders(master, cwd):
     for widget in master.winfo_children():        # Clears frame
         widget.destroy()
+
+    move_up_button = ttk.Button(master,
+                                text = f'   ..',
+                                command = lambda: [chdir(f'..'), list_folders(master, getcwd())],
+                                width = 30)
+    move_up_button.pack(ipady = 5)
 
     for folder_names in sorted(next(walk(cwd))[1]):        # and places buttons
         cb.draw_folder_button(master, cwd, folder_names, master)
